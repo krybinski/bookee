e1f808eb2a14246342e5c705dc957b50e599bd0e
Kamil Rybiński <kamilryba19@gmail.com>
2019-07-04 08:11:59 +0200

    dotenv added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  providers: [AppService, UsersService, ConfigService],
 })
 export class AppModule {}
